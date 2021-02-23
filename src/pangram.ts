9b2e78da71b6bcdc911c4e796d0c26284b8110b2
Vincent Couturier <vincent.couturier@whoz.com>
2021-02-23 13:44:02 +0100

    clean code of kata pangram.ts

--- src/pangram.ts
+++ src/pangram.ts
@@ -1,7 +1,7 @@
 export const isPangram = (phrase: string): boolean => {
     const ALPHABET_SIZE=26
     const phraseInLowerCase = phrase.toLowerCase()
-    const listOfEmptyCharacters = Array.from(new Array(ALPHABET_SIZE)) // because new Array create an empty array with length ALPHABET_SIZE even if there's no real value in the array.
+    const listOfEmptyCharacters : string[] = Array.from(new Array(ALPHABET_SIZE)) // because new Array create an empty array with length ALPHABET_SIZE even if there's no real value in the array.
     const alphabet = listOfEmptyCharacters.map((_, letterIndex)=>String.fromCharCode("a".charCodeAt(0)+letterIndex))
     return alphabet.every((it) => phraseInLowerCase.includes(it))
 }
